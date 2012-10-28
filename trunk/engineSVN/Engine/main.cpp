214e7cb5a7f9957c244fdb1ea9a410a2e0904cef
caramizaru.horea.alexandru <caramizaru.horea.alexandru@baa69368-f525-00bf-084b-3e5fc91d10de>
2012-10-28 10:35:32 +0000

    update

--- trunk/engineSVN/Engine/main.cpp
+++ trunk/engineSVN/Engine/main.cpp
@@ -12,25 +12,6 @@
 /**************************************************/
 
 // EVENT TESTING
-class Zambet;
-class Incruntare;
-class Ana : public TemplateObserver<Zambet> , public TemplateObserver<Incruntare> ,
-            public virtual referenceCounting , public virtual Mutex {
-public:
-    typedef SmartPtr<Ana> Ptr;
-    virtual void Event( Zambet * ev ) {
-        printf("mouse event\n");
-    }
-    
-    virtual void Event( Incruntare * ev ) {
-        printf("tasta Event\n");
-    }
-};
-
-class Horea : public Mutex , public referenceCounting , public CThrowEvents {
-public:
-    typedef SmartPtr<Horea> Ptr;
-};
 
 class Zambet : public IEvent {
     
@@ -40,88 +21,51 @@ class Incruntare : public IEvent {
     
 };
 
+class Anca :    public TemplateObserver<Zambet> , public TemplateObserver<Incruntare> ,
+                public virtual referenceCounting , public virtual Mutex {
+public:
+    
+    virtual void Event( Zambet * ev ) {
+        printf("mouse event\n");
+    }
+    
+    virtual void Event( Incruntare * ev ) {
+        printf("tasta Event\n");
+    }
+                
+    typedef SmartPtr<Anca> Ptr;
+};
+
+class Horea : public Mutex , public referenceCounting , public CThrowEvents {
+public:
+    typedef SmartPtr<Horea> Ptr;
+};
+
+
+
 /**************************************************/
 
 int main(int argc, const char * argv[])
 {
 
-    //int t = 8;
-    //int &tt = 5;
-    //const int & ttt = 6;
-    
-    /*
-    TestPtr cla( new Test() );
-    
-    TestPtr cla2 (cla ) ;
 
-    TestPtr cla4 = new Test();
-    
-    TestPtr& cla3 = cla2;
-    
-    cla->a = 4;
-
-    ff( cla );
-    
-    ff( new Test() );
-    
-    ff( TestPtr( new Test()) );
-     */
-
-    //ManagerBUFFERS()->Add("Anca");
-    //SourceBufferSPtr a = ManagerBUFFERS()->Search("Anca");
-    //std::cout<<a->Name;
-    //ManagerBUFFERS()->Delete("Anca");
-    
-    //ManagerSOURCES()
-    
-    //typedef SingletonThreadSafe<ManagerSources> aa;
-    
-    //( SmartPtr<ManagerSources>() = ManagerSOURCES() )->Delete("Alba");
-    //aa bb( ManagerSOURCES() );
-    
-    //ManagerSources * ppp = ManagerSOURCES();
-    
-    //static_cast<aa>( aa( ppp ) );
-   // aa bb( ManagerSOURCES() );
-    //aa()->Search("ANA");
-    
-    //GetManagerBuffersThreadSafe()->Search("Anca");
-    
-    //SmartPtr<ManagerSources>() = ManagerSOURCES();
-    
-    
-    
-    
-    // test zip
-   /* ZipInstance zip("Test1.zip");
-    printf("%s", zip.GetName().c_str());
-    for (; !zip.EOZip(); zip.GoToNextFile())
-    {
-        zip.OpenCurrentFile();
-        CFile *bla = (CFile*)zip.ReadCurrentFile();
-        zip.GetCurrentFileInfo().print();
-        bla->Save("");
-        zip.CloseCurrentFile();
-    }
-    */
     
     printf("\n ENGINE OK ! \n");
     
     /***********************************************/
     
     // EVENTS TESTING (random testing)
-    Ana::Ptr ana( new Ana() );
+    Anca::Ptr anca( new Anca() );
     
     Horea::Ptr horea( new Horea() );
     
-    dynamic_cast<TemplateObserver<Zambet> *>(*ana)->RegisterTo(*horea);
-    dynamic_cast<TemplateObserver<Incruntare>* >(*ana)->RegisterTo(*horea);
+    dynamic_cast<TemplateObserver<Zambet> *>(*anca)->RegisterTo(*horea);
+    dynamic_cast<TemplateObserver<Incruntare>* >(*anca)->RegisterTo(*horea);
     
     horea->addEvent(new Zambet() );
     
     horea->Dispatch();
     
-    // TO COMPLETE
     
     /***********************************************/
     // create threads
@@ -165,7 +109,8 @@ int main(int argc, const char * argv[])
         delete ff;
         zip.CloseCurrentFile();
     }
-     */
+    */
+     
     return 0;
 }
 
